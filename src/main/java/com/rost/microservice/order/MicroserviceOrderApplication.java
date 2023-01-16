731a5c46449b9fc60752e891a084fc07647d3a03
Rost <coderost@yandex.ru>
2023-01-16 13:19:16 +0300

    Abolished "item" model and corresponding code.

--- src/main/java/com/rost/microservice/order/MicroserviceOrderApplication.java
+++ src/main/java/com/rost/microservice/order/MicroserviceOrderApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroserviceOrderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceOrderApplication.class, args);
 	}
-
 }
